fa63ef1ff34c3d9085c00397d681b9ed826cfa3c
Guillaume Laforge <glaforge@gmail.com>
2021-04-12 14:52:23 +0000

    Ongoing work on loading books from the Web API, and styling the output

--- appengine-frontend/public/js/app.js
+++ appengine-frontend/public/js/app.js
@@ -2,5 +2,24 @@ console.log('App started');
 
 document.addEventListener("DOMContentLoaded", async function(event) {
     const response = await fetch('https://run-crud-sh43zgzkgq-ew.a.run.app/books');
-    console.log(response.json());
+
+    const books = await response.json();
+    console.log(books);
+
+    for (let [key, value] of response.headers) {
+        console.log(`${key} = ${value}`);
+    }
+
+    const library = document.getElementById('library');
+    const template = document.getElementById('book-card');
+    for (let book of books) {
+        const bookCard = template.content.cloneNode(true);
+        bookCard.querySelector('slot[name=author]').innerText = book.author;
+        bookCard.querySelector('slot[name=title]').innerText = book.title;
+        bookCard.querySelector('slot[name=year]').innerText = book.year;
+        bookCard.querySelector('slot[name=isbn]').innerText = book.isbn;
+        library.appendChild(bookCard);
+    }
+
+    
 });
