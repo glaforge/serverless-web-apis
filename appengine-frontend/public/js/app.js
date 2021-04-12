3b650eee38e5340924220b8787dcbe9a43720245
Guillaume Laforge <glaforge@gmail.com>
2021-04-12 17:16:25 +0000

    Adding EAN13 barcode support

--- appengine-frontend/public/js/app.js
+++ appengine-frontend/public/js/app.js
@@ -1,25 +1,39 @@
 console.log('App started');
 
 document.addEventListener("DOMContentLoaded", async function(event) {
-    const response = await fetch('https://run-crud-sh43zgzkgq-ew.a.run.app/books');
+    var page = 0;
 
+    const moreButton = document.getElementById('more-button');
+    moreButton.addEventListener('sl-focus', event => {
+        console.log('Button clicked');
+        moreButton.blur();
+
+        appendMoreBooks(page++);
+    });
+});
+
+async function appendMoreBooks(page) {
+    const response = await fetch('https://run-crud-sh43zgzkgq-ew.a.run.app/books?page=' + page);
     const books = await response.json();
-    console.log(books);
-
-    for (let [key, value] of response.headers) {
-        console.log(`${key} = ${value}`);
-    }
 
     const library = document.getElementById('library');
     const template = document.getElementById('book-card');
     for (let book of books) {
         const bookCard = template.content.cloneNode(true);
-        bookCard.querySelector('slot[name=author]').innerText = book.author;
-        bookCard.querySelector('slot[name=title]').innerText = book.title;
-        bookCard.querySelector('slot[name=year]').innerText = book.year;
-        bookCard.querySelector('slot[name=isbn]').innerText = book.isbn;
-        library.appendChild(bookCard);
-    }
 
-    
-});
+        bookCard.querySelector('slot[name=title]').innerText = book.title;
+        bookCard.querySelector('slot[name=language]').innerText = book.language;
+        bookCard.querySelector('slot[name=author]').innerText = book.author;
+        bookCard.querySelector('slot[name=year]').innerText = book.year;
+        
+        const img = document.createElement('img');
+        img.setAttribute('id', book.isbn);
+        img.setAttribute('class', 'img-barcode-' + book.isbn)
+        //bookCard.querySelector('slot[name=barcode]').parentElement.appendChild(img);
+        bookCard.querySelector('slot[name=barcode]').appendChild(img);
+
+        library.appendChild(bookCard);
+
+        JsBarcode('.img-barcode-' + book.isbn).EAN13(book.isbn, {fontSize: 18, textMargin: 0, height: 60}).render();
+    }
+}
